8c1ed0e1d16c654e776f3456c4957994ed5d4d77
igorvaz <igor.vaz@bigdatacorp.com.br>
2018-01-07 19:26:32 -0200

    Reading IP from input file, parsing and saving to output file

--- IPAnalyzer.py
+++ IPAnalyzer.py
@@ -5,23 +5,19 @@ SHODAN_API_KEY = "pL2Xta897UmrCxHByAlkB08Xp5PjHM8h"
 
 api = shodan.Shodan(SHODAN_API_KEY)
 
-file = open("ip.txt","r")
+ipFile = open("ip.txt","r")
 
-for line in file:
+IpInfoFile = open("ipInfo.txt","w")
+
+for line in ipFile:
 
 	# Lookup the host
 	host = api.host(line)
 
 	# Print general info
-	print ("""
-			IP: %s
-			Organization: %s
-			Operating System: %s
-			country: %s
-			city: %s
-			longitude: %s
-			latitude: %s
-	""" % (host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a'), host['country_code'],host['city'], host['latitude'], host['longitude']))
+	print ("IP: {0}\ncountry: {1}\ncity: {2}\nlongitude: {3}\nlatitude: {4}".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
+	IpInfoFile.write("IP: {0}\tcountry: {1}\tcity: {2}\tlongitude: {3}\tlatitude: {4}\n".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
+	IpInfoFile.flush()
 	time.sleep(2)
 
 
