9c3dccf27a9130d8e638a55459434afeccca70af
igorvaz <igor.vaz@bigdatacorp.com.br>
2018-01-07 21:14:15 -0200

    Integrating ipAnalyzer with google maps api

--- IPAnalyzer.py
+++ IPAnalyzer.py
@@ -1,5 +1,11 @@
 import shodan
 import time
+import requests
+import json
+
+GOOGLEMAPS_API_KEY = "AIzaSyCvdA9pmAodt1JknfuvTYoIuf9K5IUxwQo"
+
+GOOGLEMAPS_API_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng={0},{1}&key={2}"
 
 SHODAN_API_KEY = "pL2Xta897UmrCxHByAlkB08Xp5PjHM8h"
 
@@ -9,21 +15,28 @@ ipFile = open("ip.txt","r")
 
 IpInfoFile = open("ipInfo.txt","w")
 
+UniversitiesFIle = open("UniversitiesAddresses.txt","r")
+
 for line in ipFile:
 
 	# Lookup the host
 	host = api.host(line)
 
-	# Print general info
-	print ("IP: {0}\ncountry: {1}\ncity: {2}\nlongitude: {3}\nlatitude: {4}".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
-	IpInfoFile.write("IP: {0}\tcountry: {1}\tcity: {2}\tlongitude: {3}\tlatitude: {4}\n".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
-	IpInfoFile.flush()
+	if(host['latitude'] == None or host['longitude'] == None):
+		continue
+	else:
+		#TEST FUNDAO
+		response = requests.get(GOOGLEMAPS_API_URL.format("-22.8596589","-43.2303241",GOOGLEMAPS_API_KEY))
+		#REAL GET
+#		response = requests.get(GOOGLEMAPS_API_URL.format(host['latitude'],host['longitude'],GOOGLEMAPS_API_KEY))
+		json_data = json.loads(response.text)
+		# print formatted address for debug
+		for addressComponent in json_data['results']:
+			print(addressComponent['formatted_address'])
+		# Print general info
+		print ("IP: {0}\ncountry: {1}\ncity: {2}\nlongitude: {3}\nlatitude: {4}".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
+
+
+		IpInfoFile.write("IP: {0}\tcountry: {1}\tcity: {2}\tlongitude: {3}\tlatitude: {4}\n".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
+		IpInfoFile.flush()
 	time.sleep(2)
-
-
-# Print all banners
-# for item in host['data']:
-#         print ("""
-#                 Port: %s
-#                 Banner: %s
-#        """ % (item['port'], item['data']))
