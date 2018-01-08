2ca06753dbd934aaa554d70683edc43634bbd4cc
igorvaz <igor.vaz@bigdatacorp.com.br>
2018-01-07 22:53:57 -0200

    Adding try/catch and log error file

--- BlockAnalyzer.py
+++ BlockAnalyzer.py
@@ -12,28 +12,36 @@ ipFile = open("ip.txt","w")
 
 delay = 10
 
+analyzerError = open("blockError.txt", "w")
+
 while True:
-	print("Getting latest block hash. . .\n")
-	response = requests.get(LATEST_BLOCK_API)
-	if response == None or response == "":
-		print("Empty response on latest block hash! Trying again!\n")
-		pass
-	else:
-		latestBlockHash = response.text
-		#Check if its the same ip viewed at last time to minimize duplicate
-		if lastBlockHash != latestBlockHash:
-			print("Getting latest block info. . .\n")
-			response = requests.get(BLOCK_INFO.format(latestBlockHash))
-			if response == None or response == "":
-				print("Empty response on latest block info! Trying again!\n")
-				pass
-			else:
-				json_data = json.loads(response.text)
-				ipFile.write(json_data["relayed_by"]+"\n")
-				#update viewd block
-				lastBlockHash = latestBlockHash
-		else:
-			print("Block recent viewd!\n")
+	try:
+		print("Getting latest block hash. . .\n")
+		response = requests.get(LATEST_BLOCK_API)
+		if response == None or response == "":
+			print("Empty response on latest block hash! Trying again!\n")
 			pass
+		else:
+			latestBlockHash = response.text
+			#Check if its the same ip viewed at last time to minimize duplicate
+			if lastBlockHash != latestBlockHash:
+				print("Getting latest block info. . .\n")
+				response = requests.get(BLOCK_INFO.format(latestBlockHash))
+				if response == None or response == "":
+					print("Empty response on latest block info! Trying again!\n")
+					pass
+				else:
+					json_data = json.loads(response.text)
+					ipFile.write(json_data["relayed_by"]+"\n")
+					ipFile.flush()
+					#update viewd block
+					lastBlockHash = latestBlockHash
+			else:
+				print("Block recent viewd!\n")
+				pass
+	except Exception as e:
+		print("Unknow error :{0}".format(e))
+		analyzerError.write("Error:{0} \t response: {1}\n".format(e,response.text))
+		analyzerError.flush()
 	print("Waiting {0} seconds. . .\n".format(delay))
 	time.sleep(delay)
