771691fdffdc81bac5898ca0af37144a1003d8c4
igorvaz <igor.vaz@bigdatacorp.com.br>
2018-01-09 23:33:41 -0200

    [IPAnalyzer] Matching LatLong to try check if its a public university IP

--- IPAnalyzer.py
+++ IPAnalyzer.py
@@ -3,9 +3,11 @@ import time
 import requests
 import json
 
-GOOGLEMAPS_API_KEY = "AIzaSyCvdA9pmAodt1JknfuvTYoIuf9K5IUxwQo"
+from geopy.distance import great_circle
 
-GOOGLEMAPS_API_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng={0},{1}&key={2}"
+#GOOGLEMAPS_API_KEY = "AIzaSyCvdA9pmAodt1JknfuvTYoIuf9K5IUxwQo"
+
+#GOOGLEMAPS_API_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng={0},{1}&key={2}"
 
 SHODAN_API_KEY = "pL2Xta897UmrCxHByAlkB08Xp5PjHM8h"
 
@@ -15,28 +17,34 @@ ipFile = open("ip.txt","r")
 
 IpInfoFile = open("ipInfo.txt","w")
 
-UniversitiesFIle = open("UniversitiesAddresses.txt","r")
+UniversitiesFile = open("UniversitiesAddresses.txt","r")
+
+universitiesLatLong = []
+
+for line in UniversitiesFile:
+	split = line.replace("\n","").split(",")
+	universitiesLatLong.append([float(split[0]), float(split[1])])
 
 for line in ipFile:
+	try:
+	
+		ip = line.split("\t")[0].split(":")[0]
+		# print(ip)
+		# exit()
+		# Lookup the host
+		host = api.host(ip)
 
-	# Lookup the host
-	host = api.host(line)
+		if(host['latitude'] == None or host['longitude'] == None):
+			continue
+		else:
 
-	if(host['latitude'] == None or host['longitude'] == None):
-		continue
-	else:
-		#TEST FUNDAO
-		response = requests.get(GOOGLEMAPS_API_URL.format("-22.8596589","-43.2303241",GOOGLEMAPS_API_KEY))
-		#REAL GET
-#		response = requests.get(GOOGLEMAPS_API_URL.format(host['latitude'],host['longitude'],GOOGLEMAPS_API_KEY))
-		json_data = json.loads(response.text)
-		# print formatted address for debug
-		for addressComponent in json_data['results']:
-			print(addressComponent['formatted_address'])
-		# Print general info
-		print ("IP: {0}\ncountry: {1}\ncity: {2}\nlongitude: {3}\nlatitude: {4}".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
-
-
-		IpInfoFile.write("IP: {0}\tcountry: {1}\tcity: {2}\tlongitude: {3}\tlatitude: {4}\n".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
-		IpInfoFile.flush()
-	time.sleep(2)
+			ipLatLong = (host['latitude'],host['longitude'])
+			for UniversityLatLong in universitiesLatLong:
+				distance = great_circle((UniversityLatLong[0],UniversityLatLong[1]),ipLatLong).miles
+				if(distance < 1.0):
+					print("Possible positive: {0}".format(ip))
+					
+			print ("IP: {0}\ncountry: {1}\ncity: {2}\nlatitude: {3}\nlongitude: {4}".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
+	except Exception as e:
+		print("Unknow error :{0}".format(e))
+	time.sleep(5)
