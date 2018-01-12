f0360468dcf2a8815cbc1656fe3322f1a9b37006
igorvaz <igor.vaz@bigdatacorp.com.br>
2018-01-12 08:38:58 -0200

    Updating all files

--- IPAnalyzer.py
+++ IPAnalyzer.py
@@ -19,32 +19,45 @@ IpInfoFile = open("ipInfo.txt","w")
 
 UniversitiesFile = open("UniversitiesAddresses.txt","r")
 
+statsFile =  open("stats.txt","w")
+
 universitiesLatLong = []
 
 for line in UniversitiesFile:
 	split = line.replace("\n","").split(",")
 	universitiesLatLong.append([float(split[0]), float(split[1])])
 
+ipCount = {}
+CountryCount = {}
+
 for line in ipFile:
 	try:
 	
 		ip = line.split("\t")[0].split(":")[0]
-		# print(ip)
-		# exit()
-		# Lookup the host
+
 		host = api.host(ip)
+
+		if ip in ipCount:
+			ipCount[ip] += 1
+		else:
+			ipCount[ip] = 1
+
+		if host['country_code'] in CountryCount:
+			CountryCount[host['country_code']] += 1
+		else:
+			CountryCount[host['country_code']] = 1
 
 		if(host['latitude'] == None or host['longitude'] == None):
 			continue
 		else:
-
 			ipLatLong = (host['latitude'],host['longitude'])
 			for UniversityLatLong in universitiesLatLong:
 				distance = great_circle((UniversityLatLong[0],UniversityLatLong[1]),ipLatLong).miles
 				if(distance < 0.12):
 					print("Possible positive: {0}".format(ip))
-
-			print ("IP: {0}\ncountry: {1}\ncity: {2}\nlatitude: {3}\nlongitude: {4}".format(host['ip_str'], host['country_code'],host['city'], host['latitude'], host['longitude']))
 	except Exception as e:
 		print("Unknow error :{0}".format(e))
 	time.sleep(5)
+statsFile.write(json.dumps(ipCount) + "\n")
+statsFile.write(json.dumps(CountryCount) + "\n")
+statsFile.flush()
